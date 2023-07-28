df42ba8e776f0ac774cee0e373ef80a32b20de5f
Nino Angelo Lumapac <nlumapac@gmail.com>
2023-07-28 10:25:40 -0600

    Delete a container

--- azure-blob/Program.cs
+++ azure-blob/Program.cs
@@ -89,5 +89,13 @@ static async Task ProcessAsync()
     Console.WriteLine("Press 'Enter' to continue.");
     Console.ReadLine();
 
-    
+    // Delete the container and clean up local files created
+    Console.WriteLine("\n\nDeleting blob container...");
+    await containerClient.DeleteAsync();
+
+    Console.WriteLine("Deleting the local source and downloaded files...");
+    File.Delete(localFilePath);
+    File.Delete(downloadFilePath);
+
+    Console.WriteLine("Finished cleaning up.");
 }
