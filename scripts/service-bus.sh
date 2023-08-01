bba496374cc54ff64a034bf0e6f79512a75ff18d
Nino Angelo Lumapac <nlumapac@gmail.com>
2023-08-01 11:09:28 -0600

    Create service bus script

---
+++ scripts/service-bus.sh
@@ -0,0 +1,17 @@
+# Create variables for the Service Bus namespace and queue names
+myLocation=canadacentral
+myNameSpaceName=az204svcbus$RANDOM
+
+# Crate a resource group
+az group create --name az204-svcbus-rg --location $myLocation
+
+# Create a Service Bus namespace
+az servicebus namespace create \
+    --resource-group az204-svcbus-rg \
+    --name $myNameSpaceName \
+    --location $myLocation
+
+# Create a Service Bus queue
+az servicebus queue create --resource-group az204-svcbus-rg \
+    --namespace-name $myNameSpaceName \
+    --name az204-queue
